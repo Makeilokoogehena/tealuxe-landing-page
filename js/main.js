477b3347df0906dad777919984999c94e0fd3760
Makeilokoogehena <studworkse@gmail.com>
2024-02-18 21:11:07 +0300

    FEAT: create logic background for burger menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const menuButton = document.querySelector(".menu__button");
 const menuClose = document.querySelector(".menu__close");
 const menuList = document.querySelector(".menu__list");
+const body = document.querySelector(".body");
 
 menuButton.addEventListener("click", () => {
   menuList.classList.add("menu__list-open");
+  body.style.setProperty("--overlay-display", "block");
 });
 
 menuClose.addEventListener("click", () => {
   menuList.classList.remove("menu__list-open");
+  body.style.setProperty("--overlay-display", "none");
 });
