cd7fa9bf97fdc1364a0bc4c14f41f95c816ed7a0
Makeilokoogehena <studworkse@gmail.com>
2024-03-08 23:23:38 +0300

    FEAT: add logic of hide for mobile menu, overlay HTML-elements by click, keydown, resize events

--- js/main.js
+++ js/main.js
@@ -3,12 +3,40 @@ const menuClose = document.querySelector(".menu__close");
 const menuList = document.querySelector(".menu__list");
 const body = document.querySelector(".body");
 
-menuButton.addEventListener("click", () => {
+function onOpenButtonClick() {
   menuList.classList.add("menu__list-open");
   body.style.setProperty("--overlay-display", "block");
-});
+}
 
-menuClose.addEventListener("click", () => {
+function onCloseButtonClick() {
   menuList.classList.remove("menu__list-open");
   body.style.setProperty("--overlay-display", "none");
-});
+}
+
+function onDocumentClick(e) {
+  if (!menuList.classList.contains("menu__list-open")) return;
+  if (menuList.contains(e.target) || menuButton.contains(e.target)) {
+    console.log("click");
+    return;
+  }
+  onCloseButtonClick();
+}
+
+function onDocumentKeydown(e) {
+  if (!menuList.classList.contains("menu__list-open")) return;
+  if (e.code === "Escape") {
+    console.log("keydown");
+    onCloseButtonClick();
+  }
+}
+
+function onWindowResize() {
+  if (!menuList.classList.contains("menu__list-open")) return;
+  if (window.innerWidth >= 770) onCloseButtonClick();
+}
+
+menuButton.addEventListener("click", onOpenButtonClick);
+menuClose.addEventListener("click", onCloseButtonClick);
+document.addEventListener("click", (obj) => onDocumentClick(obj));
+document.addEventListener("keydown", (obj) => onDocumentKeydown(obj));
+window.addEventListener("resize", onWindowResize);
