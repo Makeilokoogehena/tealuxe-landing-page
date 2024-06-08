170183ad67d3cda7fc7f7ea708bfd2ef55b17548
Makeilokoogehena <studworkse@gmail.com>
2024-06-09 00:14:04 +0300

    FEAT: create DOM dynamic elements at main-blog list by using JS

--- main.js
+++ main.js
@@ -38,3 +38,68 @@ menuClose.addEventListener("click", onCloseButtonClick);
 document.addEventListener("click", (obj) => onDocumentClick(obj));
 document.addEventListener("keydown", (obj) => onDocumentKeydown(obj));
 window.addEventListener("resize", onWindowResize);
+
+//
+
+const dataBlog = [
+  {
+    id: 1,
+    title: "Tea Time Travels: Funny Moments from Around the World",
+    author: "Robinson Crusoe",
+    image: "img1",
+  },
+  {
+    id: 2,
+    title: "The Role of Tea in Mental Health",
+    author: "Joker",
+    image: "img3",
+  },
+  {
+    id: 3,
+    title: "Tea-rific Tales: Hilarious Stories from Tea Lovers",
+    author: "by Casper the Friendly Ghost",
+    image: "img2",
+  },
+
+  {
+    id: 4,
+    title: "Tea and Caffeine: Effects on the Brain and Behavior",
+    author: "Author Authorius",
+    image: "img4",
+  },
+];
+
+//
+const menuBlogList = document.querySelector(".main-blog__list");
+function insertAfterBegin() {
+  const result = menuBlogList.insertAdjacentHTML(
+    "afterbegin",
+    generateListTemplate(item)
+  );
+  return result;
+}
+//
+
+const generateListTemplate = (item) => {
+  const template = document.createElement("li");
+  template.classList.add("main-blog__item");
+  template.setAttribute("id", `${item.id}`);
+  template.innerHTML = `<img
+  class="main-blog__item-image"
+  src="assets/images/blog-${item.image}.png"
+  alt="the role of tea"
+/>
+<div class="main-blog__item-content">
+  <h3 class="main-blog__content-title">
+  ${item.title}
+  </h3>
+  <cite class="main-blog__author">by ${item.author}</cite>
+  <a class="main-blog__item-link" href="#">Read </a>
+</div>`;
+  return template;
+};
+
+dataBlog.forEach((obj) => {
+  const item = generateListTemplate(obj);
+  menuBlogList.appendChild(item);
+});
